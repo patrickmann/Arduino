9803a0b66bd162246c2ddb360b2175c49bde72b5
Patrick Mann <patrickmann@hotmail.com>
2019-12-01 22:03:11 +0100

    SD card init

--- sketch_PU/sketch_PU.ino
+++ sketch_PU/sketch_PU.ino
@@ -2,7 +2,10 @@
 #include <DallasTemperature.h>
 #include <DS1307.h>
 #include <rgb_lcd.h>
+#include <SPI.h>
+#include <SD.h>
 
+const int selectedChip = 4;
 const int sensorCount = 5;
 const DeviceAddress sensors[sensorCount] = {  // Previously determined
   {0x28, 0xAA, 0x98, 0xDA, 0x53, 0x14, 0x01, 0x08},
@@ -11,15 +14,12 @@ const DeviceAddress sensors[sensorCount] = {  // Previously determined
   {0x28, 0xAA, 0xBF, 0x3E, 0x54, 0x14, 0x01, 0x9C},
   {0x28, 0x9A, 0xA5, 0xCF, 0x5E, 0x14, 0x01, 0x24}
 };
-const char* sensorNames[sensorCount] = { "bath", "bottle 1", "bottle 2", "bottle 3", "bottle 4"};
+const char* sensorNames[sensorCount] = { "w", "b1", "b2", "b3", "b4"};
 const int bottleMinIndex = 1, bottleMaxIndex = 4;
 float lastTemp[sensorCount] = {999, 999, 999, 999, 999};
 float currTemp[sensorCount] = {999, 999, 999, 999, 999};
-float diffTemp[sensorCount] = {0, 0, 0, 0, 0};
 float totalPu[sensorCount] = {0, 0, 0, 0, 0};
 bool sensorValid[sensorCount] = {true, true, true, true, true};
-int sensorFlagged[sensorCount] = {0, 0, 0, 0, 0};
-const int maxStrikes = 3; // invalidate sensor after maxStrikes problems
 
 // Setup a oneWire instance to communicate with any OneWire devices
 #define ONE_WIRE_BUS_PIN 2
@@ -48,24 +48,32 @@ long diffMillis;
 enum State {initializing, waiting, accumulating, cooling} state = initializing;
 bool errorState = false;
 char sprintfBuffer[80];
+File dataFile;
 
 void setup() {
   clock.begin();
   lcd.begin(16, 2); // Display initialisieren - 2 Zeilen mit jeweils 16 Zeichen
 
   Serial.begin(9600);
-  Serial.print("Initializing Temperature Control Library Version ");
-  Serial.println(DALLASTEMPLIBVERSION);
   dallas.begin();
 
   // set the resolution to 10 bit (Can be 9 to 12 bits .. lower is faster)
   for (int i = 0 ; i < sensorCount; i++) {
     dallas.setResolution(sensors[i], 10);
   }
+
+  if (SD.begin(selectedChip)) {
+    Serial.println(F("SD-Card initialized."));
+  }
+  else {
+    Serial.println(F("SD-Card failed"));
+  }
+  String line = String("Hallo Welt");
+  writeToSD(line);
 }
 
 void loop() {
-  Serial.println(" *********** Cycle Reset ************");
+  Serial.println(F("*** Cycle Reset"));
   if (!errorState) {
     // Don't erase prior error color
     lcd.setRGB(128, 128, 128);
@@ -76,23 +84,22 @@ void loop() {
 
   while (bottleTemp < startStopAccumulating) {
     Serial.print(bottleTemp);
-    Serial.println("C: Waiting to warm up ...");
+    Serial.println(F("C: Waiting to warm up ..."));
     lcdPrint1("Warming up");
     sprintf(sprintfBuffer, "%sC", tempToStr(bottleTemp));
     lcdPrint2(sprintfBuffer);
 
     delay(waitInterval);
     snapTemp();
-    snapDiff();
     sensorCheck();
     bottleTemp = bottleMinTemp(currTemp);
   }
-  
-  Serial.println("************ Cycle start: accumulating PUs **************");
+
+  Serial.println(F("*** Cycle start: accumulating"));
   errorState = false;
   lcd.setColor(BLUE);
   if (nValidSensors() < 2) {
-    Serial.println("ERROR - starting pasteurization with insufficient sensors!");
+    Serial.println(F("ERROR - insufficient sensors!"));
     lcd.setColor(RED);
     errorState = true;
     delay(10000);
@@ -112,7 +119,7 @@ void loop() {
 
     if (minPu >= puTarget) {
       state = cooling;
-      Serial.println(" *************** Cycle end: target PU reached *****************");
+      Serial.println(F(" *** Cycle end: target PU reached"));
       if (!errorState) {
         lcd.setColor(GREEN);
       }
@@ -121,7 +128,7 @@ void loop() {
 
     else if (bottleTemp < startStopAccumulating) {
       state = waiting;
-      Serial.println(" ********* Cycle interrupted prematurely ************");
+      Serial.println(F(" *** Cycle interrupted prematurely"));
       lcd.setColor(RED);
       errorState = true;
       lcdPrint("Aborted", "prematurely");
@@ -141,7 +148,6 @@ void loop() {
 // Delay for measure interval, then get temp and PUs
 void snapPu() {
   snapTemp();
-  snapDiff();
   sensorCheck();
   bottleTemp = bottleMinTemp(currTemp);
 
@@ -152,20 +158,20 @@ void snapPu() {
   int delayMillis = measureInterval - diffMillis;
   if (delayMillis < 0) {
     delayMillis = 0;
-    Serial.print("WARNING measure interval exceeded - neg. delay");
+    Serial.print(F("WARNING neg. delay"));
   }
   else {
-    Serial.print("Delaying: "); Serial.println(measureInterval - diffMillis);
+    Serial.print(F("Delaying: ")); Serial.println(measureInterval - diffMillis);
     delay(measureInterval - diffMillis);
   }
 
   minPu = addPu();
   lastMillis = millis(); // timestamp immediately after processing the last interval
-  Serial.print("Min PU="); Serial.print(minPu);
-  Serial.print("   PU: ");
+  Serial.print(F("Min PU=")); Serial.print(minPu);
+  Serial.print(F("   PU: "));
   for (int i = bottleMinIndex; i <= bottleMaxIndex; i++) {
     Serial.print(totalPu[i]);
-    Serial.print("  ");
+    Serial.print(F("  "));
   }
   Serial.println();
 
@@ -179,15 +185,6 @@ void snapTemp() {
     currTemp[i] = getTemperature(i);
   }
   Serial.println();
-}
-
-void snapDiff() {
-  for (int i = 0; i < sensorCount; i++) {
-    diffTemp[i] = currTemp[i] - lastTemp[i];
-    if (diffTemp[i] < 0) {
-      diffTemp[i] *= -1;
-    }
-  }
 }
 
 // Increment PUs and return the lowest valid total PU value
@@ -238,18 +235,18 @@ void sensorCheck() {
   float min = bottleMinTemp(currTemp);
   float max = bottleMaxTemp(currTemp);
   if (max - min > bottleMaxDiff) {
-    Serial.println("High discrepancy - seeking unused sensors");
+    Serial.println(F("High discrepancy - seeking unused sensors"));
     Serial.print(max - min);
-    Serial.println("C");
+    Serial.println(F("C"));
 
     for (int i = bottleMinIndex; i <= bottleMaxIndex; i++) {
       if (sensorValid[i]) {
         if (currTemp[i] - min < sensorThreshold) {
           // stuck at low temp - assuming not in use
           sensorValid[i] = false;
-          Serial.print("Invalidated stuck sensor ");
+          Serial.print(F("Invalidated stuck sensor "));
           printTemp(i, currTemp[i]);
-          Serial.println("*****");
+          Serial.println();
         }
       }
     }
@@ -261,9 +258,9 @@ void sensorCheck() {
     for (int i = bottleMinIndex; i <= bottleMaxIndex; i++) {
       if (!sensorValid[i] && (max - currTemp[i] < sensorThreshold)) {
         sensorValid[i] = true;
-        Serial.print("Re-enabling sensor ");
+        Serial.print(F("Re-enabling sensor "));
         printTemp(i, currTemp[i]);
-        Serial.println("*****");
+        Serial.println();
       }
     }
   }
@@ -276,13 +273,13 @@ int nValidSensors() {
       nValid++;
     }
   }
-  Serial.print("Valid sensors="); Serial.println(nValid);
-  
+  Serial.print(F("Valid sensors=")); Serial.println(nValid);
+
   // Need at least 2 valid sensors for high confidence results
   if (nValid < 2) {
-    Serial.println("WARNING less than 2 valid bottle sensors!");
+    Serial.println(F("WARNING less than 2 valid bottle sensors!"));
   }
-  return nValid; 
+  return nValid;
 }
 
 float getTemperature(int index) {
@@ -291,15 +288,9 @@ float getTemperature(int index) {
   printTemp(index, tempC);
 
   if (-127.00 == tempC) {
-    Serial.print("ERROR reading ");
-    Serial.println(sensorNames[index]);
-    sensorFlagged[index]++;
-    if (sensorFlagged[index] > maxStrikes) {
-      sensorValid[index] = false;
-      Serial.print("Invalidated disconnected sensor ");
-      printTemp(index, tempC);
-      Serial.println("*****");
-    }
+    Serial.print(F("ERROR reading ")); Serial.println(sensorNames[index]);
+    sensorValid[index] = false;
+    Serial.println(F("Invalidated disconnected sensor"));
   }
   return tempC;
 }
@@ -315,9 +306,9 @@ char* tempToStr(float temp) {
 
 void printTemp (int index, float temp) {
   Serial.print(sensorNames[index]);
-  Serial.print(": ");
+  Serial.print(F(": "));
   Serial.print(temp);
-  Serial.print("C   ");
+  Serial.print(F("C   "));
 }
 
 void lcdPrint(const char* line1, const char* line2) {
@@ -339,3 +330,13 @@ void lcdPrint2(const char* line2) {
 //Date;Time;TotalPU;Bath;Bottle1;Bottle2;Bottle3;Bottle4;PU1;PU2;PU3;PU4
 //2019-11-27;8:55:32;0,00 ;16,00 ;15,00 ;15,00 ;15,00 ;15,00 ;0,10 ;0,20 ;0,30 ;0,40
 //2019-11-27;8:55:33;0,00 ;16,00 ;15,00 ;15,00 ;15,00 ;15,00 ;0,10 ;0,20 ;0,30 ;0,40
+void writeToSD(String line) {
+  File dataFile = SD.open("test.csv", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(line); // Auf die SD-Karte schreiben
+    dataFile.close();       // Datei schließen
+    Serial.println(line);
+  } else {
+    Serial.println(F("Error opening datafile"));
+  }
+}
